05df7ddc8862f667c265b17767092a89e7ed49dd
blitty <blittynet@gmail.com>
2020-07-31 21:31:21 +0200

    Updates

---
+++ precache-manifest.96a0d5bbdbad57f58156ca1a1519f4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf00ac6499857c89cd6b44e5dca82a4c",
+    "url": "/Todo-s/index.html"
+  },
+  {
+    "revision": "f408b55255c2532bc23d",
+    "url": "/Todo-s/static/css/main.ac3c7a59.chunk.css"
+  },
+  {
+    "revision": "10966a9fcfb6aa0e1fdd",
+    "url": "/Todo-s/static/js/2.2dae4a16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-s/static/js/2.2dae4a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f408b55255c2532bc23d",
+    "url": "/Todo-s/static/js/main.27a5d6e5.chunk.js"
+  },
+  {
+    "revision": "a41b962506fa5fd3697f",
+    "url": "/Todo-s/static/js/runtime-main.6a6b9b4c.js"
+  }
+]);
